75f2b7235c9c1103545ecf0cd0acd96dfed9be77
Xavier <11111@qq.com>
2022-11-02 09:35:55 +0800

    完成Nacos配置中心的配置以及使用GateWay进行路由网关配置

--- mall-gateway/src/main/java/com/xavier/mall/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/xavier/mall/gateway/MallGatewayApplication.java
@@ -1,18 +1,15 @@
-package com.xavier.mall.coupon;
+package com.xavier.mall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-public class MallCouponApplication {
+public class MallGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallCouponApplication.class, args);
+        SpringApplication.run(MallGatewayApplication.class, args);
     }
 
 }
